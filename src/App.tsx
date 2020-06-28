8585a7a02cd61c8a62d39456d427c851c203504d
Franz-K55A <franzhot@gmail.com>
2020-06-28 23:03:57 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <React.Fragment>
+            <h1 css={css`
+            color: tomato;
+            text-align: center;
+            font-size: 2.5rem;
+            `}>React & docker yass</h1>
+        </React.Fragment>
+    );
 }
 
 export default App;
